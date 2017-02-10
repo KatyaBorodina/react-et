650f05646b56adab8a1787a3b28275f38dc177d6
Kateryna Borodina <kateryna.borodina@playtech.com>
2017-02-10 18:13:34 +0200

    continue

--- 3-props-and-state/src/components/AddDayForm.js
+++ 3-props-and-state/src/components/AddDayForm.js
@@ -1,4 +1,47 @@
-export const AddDayForm = () =>
-    <div>
-        <h1>Add a day</h1>
-    </div>
+import { PropTypes, Component } from 'react'
+
+export class AddDayForm extends Component {
+    render () {
+        const { resort, date, powder, backcountry } = this.props
+
+        return (
+
+            <form className='add-day-form'>
+                <label htmlFor='resort'>Resort Name</label>
+                <input
+                    type='text'
+                    id='resort'
+                    required
+                    defaultValue={resort} />
+
+                <label htmlFor='date'>Date</label>
+                <input type='date' id='date' required />
+
+                <div>
+                    <input type='checkbox' id='powder' />
+                    <label htmlFor='powder'>Powder Day</label>
+                </div>
+                <div>
+                    <input type='checkbox' id='backcountry' />
+                    <label htmlFor='backcountry'>Backcountry Day</label>
+                </div>
+
+            </form>
+        )
+    }
+}
+
+AddDayForm.defaultProps = {
+    resort: 'wferffre',
+    date: '12-12-2017',
+    powder: true,
+    backcountry: false
+}
+
+
+AddDayForm.propTypes = {
+    resort: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+    powder: PropTypes.bool.isRequired,
+    backcountry: PropTypes.bool.isRequired
+}
